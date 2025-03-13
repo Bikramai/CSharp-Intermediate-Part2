f54339abda82cca6221a745dec1bb2b714d5d150
bikram <bikra@BIKRAM-LENOVO>
2025-03-13 13:34:48 -0400

    Inheritance - Access Modifiers - still some references is not working

--- Amazon/Customer.cs
+++ Amazon/Customer.cs
@@ -5,7 +5,7 @@ namespace Amazon
     public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         public void Promote()
         {
